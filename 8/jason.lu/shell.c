af028239efb095732445388e5851b100f5396d98
jasonluX13 <jasonluX13@gmail.com>
2014-11-19 11:19:19 -0500

    Finished

---
+++ 8/jason.lu/shell.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+  while(1){
+    char prompt[4096];
+    strcpy(prompt,"Shell:");//add directory and username
+    printf("%s", prompt);
+    char input[4096];
+    fgets(input, 4096, stdin);
+    
+  }
+  
+}
