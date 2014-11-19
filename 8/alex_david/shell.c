e0e23063ff4dee8ba9bcb9a8572f8634131c2361
Alex Libman <kingalex11235@yahoo.com>
2014-11-19 14:04:47 -0500

    need to fix parsing

--- 8/alex_david/shell.c
+++ 8/alex_david/shell.c
@@ -6,6 +6,28 @@
 #include <dirent.h>
 
 int main(){
+  printf("-- _ SHELL --\n\n"); //fill _ with name or change
+  command();
+  return 0;
+}
 
+int command(){
+  printf("_$ "); //fill _ with something
+  char s[1024];
+  fgets(s,sizeof(s),stdin);
+  int n = 0;
+  char *p = s;
+  while (*p){
+    if (*p == ' '){
+      n++;
+    }
+    p++;
+  }
+  char **params = (char *)malloc(64 * n);
+  n = 0;
+  while (s){
+    params[n] = strsep(s,' ');
+  }
+  
   return 0;
 }
