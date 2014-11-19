13d2b8975066a4450de37375cd46480ae9779678
mchaiken <mirandachaiken@gmail.com>
2014-11-19 11:32:08 -0500

    ;;;;;;;;;;;;;;!!!

--- 4/miranda_jenny/shell.c
+++ 4/miranda_jenny/shell.c
@@ -1,7 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
+#include "shell.h"
 
 int i = 1;
 
@@ -10,6 +7,7 @@ void execute( char * args[] ) {
     
     if ( !strcmp(args[0], "cd") ) {
         chdir( args[1] );
+	//getcwd( cwd, sizeof(cwd) );
     }
     if ( !strcmp(args[0], "exit") ) {
         i = 0;
@@ -27,9 +25,9 @@ void execute( char * args[] ) {
 int main(){
     printf( "Welcome to the The Magic Conch Shell.\n" );
     
-    char cwd[200];
-    char input_str[100];
     
+    char input_str[100];
+    char cwd[200];
     while( i ) {
         getcwd( cwd, sizeof(cwd) );
         printf( "%s$ ", cwd );
@@ -40,25 +38,22 @@ int main(){
         
         //strchr for ;
         char *split_cmds; //holds the piece separated off from raw_str (a cmd + arg string)
-        char * cmds[10]; //cmd array
+        //char * cmds[10]; //cmd array
         int c = 0;
-        
-        while( split_cmds = strsep(&raw_str, ";") ) {
-            cmds[c] = split_cmds;
+	char *split_args; //holds the piece separated off from x
+	char * args[10]; //arg array
+        while( (split_cmds = strsep(&raw_str, ";")) ) {
             c++;
-        }
-        cmds[c] = 0;
-        
-        int x;
-        for ( x = 0; cmds[x]; x++ ) {
-            char *split_args; //holds the piece separated off from x
-            char * args[10]; //arg array
             int a = 0;
-            
-            while( split_args = strsep(&x, " ") ) {
-                args[a] = args;
-                a++;
+	    printf("cmd: %s\n",split_cmds);
+	    if (split_cmds[0] == ' '){
+	      split_cmds= &split_cmds[1];
+	    }
+            while( (split_args = strsep(&split_cmds, " ")) ) {
+	      args[a] = split_args;
+              a++;
             }
+	    args[a]=0;
             execute(args);
         }
     }
