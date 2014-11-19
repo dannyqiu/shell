ea3a95148a0936218a4caaa6f4cdfdd1e0a22697
AdamDeHovitz <adam.dehovitz@gmail.com>
2014-11-19 15:31:59 -0500

    Exit and getcwd work, adult processes seperated
    Current directory and it’s parent shown in prompt

--- 8/adam_malcom_kyle/program.c
+++ 8/adam_malcom_kyle/program.c
@@ -1,13 +1,19 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "program.h"
+
+
+void process(char * start){
+  //site of user input processing
+  while (start){
+    execute(strsep(&start, ";"));
+      
+  }
+}
 
 void execute(char * start){
   //execute: Takes a char pointer start representing the beginning f a String
   //Splits the string arguements and runs them with a child process and execvp
   //Returns nothing
-  printf("%s\n",start);
+  //printf("%s\n",start);
   char * args[10];
   int x = 0;
   char * y;
@@ -18,6 +24,25 @@ void execute(char * start){
   }
   args[x -1] = strsep(&args[x-1], "\n");
   args[x]=0;
+  if (! (strcmp("cd",args[0]) && strcmp("exit",args[0])))
+    normal_process(args);
+  else{
+    child_process(args);
+  }
+}
+void normal_process(char * args[]){
+  //For processes that require the main process to run
+  //Takes the process with args as an array
+  if (! (strcmp("cd",args[0]))){
+    printf("IMPLEMENT CD PLS\n");
+  }
+  else if (! (strcmp("exit",args[0])))
+    exit(0);
+}
+
+void child_process(char * args[]){
+  //For processes that require a child to run
+  //Takes the process with args as an array
   int f = fork();
   if (!f){
     //printf("running: %s %s\n",args[0],args[1]);
@@ -30,19 +55,22 @@ void execute(char * start){
 }
 
 int main(int argc, char *argv[]){
-
+  char directory[256];
   while(1){
-    printf("input: ");
+    getcwd(directory,256);
+    char * direct_point = &directory[0];
+    char * args[15];
+    int x = 0;
+    while (direct_point){
+      args[x] = strsep(&direct_point, "/"); 
+      x++;
+    }
+    printf("%s/%s: input: ",args[x-2],args[x-1]);
     char input[256];
     fgets(input, 256, stdin);
     printf("\n");
     char * start = &input[0];
-    char * y;
-    while (start){
-      execute(strsep(&start, ";"));
-      
-    }
-  
+    process(start);  
     
   }
   return 0;
---
+++ 8/adam_malcom_kyle/program.h
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+void process(char * start);
+void execute(char * start);
+void child_process(char * args[]);
+void normal_process(char * args[]);
