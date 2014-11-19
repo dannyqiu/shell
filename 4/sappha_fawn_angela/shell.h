992614f76abb58b61bd931c08af72934c9c65e4a
fawnwong <fawn.wong@live.com>
2014-11-19 11:00:34 -0500

    yay

---
+++ 4/sappha_fawn_angela/shell.h
@@ -0,0 +1,11 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <signal.h>
+
+void printprompt();
+void execute();
