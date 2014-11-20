a444b67e46bbc77fbfc022b3d8fadbfc838ff926
Danny Qiu <dqiu55@gmail.com>
2014-11-19 22:44:50 -0500

    Fix various odd cases

--- shell.c
+++ shell.c
@@ -1,5 +1,7 @@
 #include "shell.h"
 
+int errno_result; // Used in collaboration with errno if function fails
+
 void shell() {
     char *input = (char *) malloc(BUFFER_SIZE);
     int done = 0;
@@ -14,11 +16,11 @@ void shell() {
       char *tmp = strdup(buf);
       char* path = strstr(tmp,home);
       if(path == NULL){
-	printf("STD:%s$ ", buf);
+	printf("StD: %s ᐅ ", buf);
       }
       else{
 	memmove(path,path+strlen(home),1+strlen(path+strlen(home))); //Removes the home directory
-	printf("STD:~%s$ ", path);
+	printf("StD: ~%s ᐅ ", path);
       }
       fflush(stdout);
       done = parse_input(input);
@@ -32,6 +34,9 @@ int parse_input(char *input) {
     if (feof(stdin)) { // To handle EOF
         return 1;
     }
+    while (argv[0] == ' ') { // To clean up spaces before commands
+        argv++;
+    }
     char *cmd = strsep(&argv, " \n");
     if (strcmp(cmd, "quit") == 0 || strcmp(cmd, "exit") == 0) {
         return 1;
@@ -40,7 +45,7 @@ int parse_input(char *input) {
         if (strstr(cmd, "cd") != NULL) {
             change_directory(argv);
         }
-        else {
+        else if (strcmp(cmd, "")) { // Makes sure that the command is not empty
             printf("Running: %s %s\n", cmd, argv);
             call_cmd(cmd, argv);
         }
@@ -64,8 +69,8 @@ void call_cmd(char *cmd, char *argv) {
             ++i;
         }
         argv_buf[i] = NULL;
-        execvp(cmd, argv_buf);
-        if (errno) {
+        errno_result = execvp(cmd, argv_buf);
+        if (errno_result == -1) {
             printf("%s: %s\n", cmd, strerror(errno));
         }
         exit(0);
@@ -74,8 +79,8 @@ void call_cmd(char *cmd, char *argv) {
 
 void change_directory(char *argv) {
     char *path = strsep(&argv, " \n");
-    chdir(path);
-    if (errno) {
+    errno_result = chdir(path);
+    if (errno_result == -1) {
         printf("%s: %s\n", "cd", strerror(errno));
     }
 }
