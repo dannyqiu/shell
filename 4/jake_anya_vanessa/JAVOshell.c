3a6b8b43ace43db886c052ec22b4ea6a8b3574d6
Jake <jakeolenick@gmail.com>
2014-11-20 10:38:46 -0500

    made non-forking commands work

--- 4/jake_anya_vanessa/JAVOshell.c
+++ 4/jake_anya_vanessa/JAVOshell.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 #include "executor.h"
 
 
@@ -8,10 +8,24 @@ int main(){
 
   while(1){
 
+    char input[256];
+    printf("JAVO:> ");
+    fgets(input, sizeof(input), stdin);
+    int i = 0;
+    while(input[i] != '\n') {
+      i++;
+    }
+    input[i] = 0;
+    if(!strcmp(input,"exit")){
+      exit(-1);
+    }
+    else if(!strcmp(input,"cd")){
+      //do cd stuff
+    }
     int f = fork();
     wait();
     if (!f){
-      execute();
+      execute(input);
     }
   }
   
--- 4/jake_anya_vanessa/executor.c
+++ 4/jake_anya_vanessa/executor.c
@@ -3,25 +3,20 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int execute() {
-  char s[256];
-  printf("What would you like to do? ");
-  fgets(s, sizeof(s), stdin);
-  int i = 0;
-  while(s[i] != '\n') {
-    i++;
-  }
-  s[i] = 0;
+int execute(char s[256]) {
+
   char *s1 = s;
   char *s2;
   char *arg[256];
-  i = 0;
+  
+  int i = 0;
   while(s2) {
     s2 = strsep(&s1, " ");
     arg[i] = s2;
     i++;
   }
   arg[i+1] = 0;
+  
   execvp(arg[0], arg);
   return 0;
 }
