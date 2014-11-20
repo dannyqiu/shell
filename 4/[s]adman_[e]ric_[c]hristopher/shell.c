d70a272631acf5192d6301c2df72fef00bf4f4ac
callmesenpai <ericliao10797@hotmail.com>
2014-11-20 11:18:59 -0500

    lol committed with my shell

--- 4/[s]adman_[e]ric_[c]hristopher/shell.c
+++ 4/[s]adman_[e]ric_[c]hristopher/shell.c
@@ -11,21 +11,42 @@
 
 int main(){
   char* args=calloc(256,sizeof(char));
-
+  char** addresses=calloc(256,sizeof(char*));
+  
   char* cwd=calloc(256,sizeof(char));  
   getcwd(cwd,256);
   while (1){
     printf("%s :^) -",cwd);
     fgets(args,256,stdin);
     args = strsep(&args,"\n");
+
     char* p;
     for(p=args;*p;++p) *p= tolower(*p);
-    if (! strcmp(args,"exit")){
+    //exit
+    if (! strcmp(args,"exit"))
       exit(0);
-    }else if (! strcmp(args,"cd")){
+    //cd
+    else if (! strncmp(args,"cd",2)){
       execlp("cd","cd",args+3);
-      printf("swag you cd'd to %s",args+3);
+      chdir(args+3);
+      getcwd(cwd,256);
+    }else if(args[0]){//if they spam enter obnoxiously like eric does -eric
+      int i=0,j=1,pid=fork();
+      if (!pid){
+	addresses[0]=args;
+	do{
+	  if (args[0]==32){
+	    addresses[j++]=args+1+i;
+	    args[i]=0;
+	  }
+	}while(args[++i]);
+	execvp(args,addresses);
+      }else
+	sleep(1);
     }
+      
+    
+    
   }
 
   return 0;
