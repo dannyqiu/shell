82ae5146b7fdd0a888361b6047b946611516237a
Sisi Zimmerman <zimmerman.sisi@gmail.com>
2014-11-20 17:30:58 -0500

    .

--- 8/mary_sisi/shell.c
+++ 8/mary_sisi/shell.c
@@ -1,38 +1,40 @@
 #include <stdio.h>
-#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
 #include <sys/stat.h>
 #include <errno.h>
+#include <string.h>
 
 
 //parse
 char** parse(){
-
+ 
   char s[256];
-  fgets( s , sizeof(s) , stdin );
-  strtok(s,"\n");  
+  char * ps = s;
+  fgets(s, sizeof(s), stdin);
+  s[strlen(s)-1]='\0';
 
-  int j = 0;
-  char* temp = s;
-  
-  while( *temp){
-    if (temp == ' '){
-      j++;
-    }
-    *temp ++;
+  char ** args = (char**)malloc(sizeof(s));
+
+  args[0] = strsep(&ps," ");
+
+  int i=1;
+
+  while(ps){
+    args[i] = ps;
+    strsep(&ps," ");
+    i++;
   }
+ 
+  /*  i=0;
+  while(args[i]){
+    printf("command:  %s\t",args[i]);
+    i++;
+    }*/
 
-  char** arr = (char**)malloc(sizeof(char*) * j);
-  arr[0] = strtok(s," ");
-  char* c;
-  j = 1;
-  while(c = strtok(0," ")){
-    arr[j] = c;
-    j++;
-  }
+  return args;
 
-  return arr;
 }
 
 
@@ -53,21 +55,31 @@ int main(){
   print_promt();
 
   while(1){
-    char ** a = parse();
 
+    char ** a = parse();
+    //printf("%s\t",a[0]);
+
+    int i=0;
+
+    while(a[i]){
+      printf("command:  %s\t",a[i]);
+      i++;
+    }
+
+    /*
     if (a[0] == "exit"){
       execlp("exit","exit", NULL);
     }
     if (a[0] == "cd"){
       execvp("cd", a );
-    }
-    else{
+      }*/
+    //else{
       int f = fork();
       if( !f ){
 	execvp(a[0], a );
 	//everything else
 	//redirection
-      }      
+            
     }
 
   }
