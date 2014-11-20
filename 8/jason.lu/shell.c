b7d578b98f5d6ad9020297acf1b90354b28c0f7c
jasonluX13 <jasonluX13@gmail.com>
2014-11-20 13:57:53 -0500

    cd works

--- 8/jason.lu/shell.c
+++ 8/jason.lu/shell.c
@@ -3,41 +3,54 @@
 #include <string.h>
 #include <unistd.h>
 
-int main(){
-  while(1){
-    
-    char prompt[4096];
-    wait();
-    strcpy(prompt,"Shell:");//add directory and username
-    printf("%s", prompt);
-    char input[4096];
-    fgets(input, 4096, stdin);
-    //Parse Input
-    strtok( input, "\n");
+char** parse(char* input, char* s){
+  strtok( input, "\n");
     int count = 0;
     char *p = input;
     while(*p){
-      if(*p == ' '){
+      if(*p == *s){
 	count++;
       }
       p++;
     }
-    char * arg = strtok(input, " ");
+    char * arg = strtok(input, s);
     char ** args = (char **)malloc(sizeof(char *)* count+1);
     args[0] = arg;
     int i = 0;
     while(arg){
         i++;
-        arg = strtok(NULL, " ");
+        arg = strtok(NULL, s);
         args[i] = arg;
     }
+    return args;
+}
+
+int main(){
+  while(1){
+    //Prompt
+    char prompt[4096];
+    wait();
+    strcpy(prompt,"Shell:");//add directory and username
+    printf("%s", prompt);
+    getcwd(prompt, sizeof(prompt));
+    printf("%s$ ", prompt);
     
+    char input[4096];
+    fgets(input, 4096, stdin);
+
+    //attempt at semicolon
+    char ** superargs = parse(input,";"); 
+    
+    char ** args = parse(input," ");
+
    
+    
+    
+    //Executes input
     if(strcmp(args[0],"exit") == 0){
       exit(0);
-    }else if(strcmp(args[0],"cd\n") == 0){
-      //need to parse input
-      //add stuff here
+    }else if(strcmp(args[0],"cd") == 0){
+      chdir(args[1]);
     }else{
       int f = fork();
       if(!f){
@@ -45,14 +58,7 @@ int main(){
 	printf("Command not found\n");
       }
     }
-    /*
-    i = 0;
     
-    while(args[i]){
-      printf("%s\n",args[i]);
-      i++;
-    }
-    */
   }
   
 }
