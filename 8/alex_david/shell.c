1ef4d38c7bcd7a0fd25499d3f173037edcfe641e
Alex Libman <kingalex11235@yahoo.com>
2014-11-19 20:51:08 -0500

    fixed parsing

--- 8/alex_david/shell.c
+++ 8/alex_david/shell.c
@@ -6,7 +6,7 @@
 #include <dirent.h>
 
 int main(){
-  printf("-- _ SHELL --\n\n"); //fill _ with name or change
+  printf("-- _ SHELL --\n\n"); //fill _ with name or change]
   command();
   return 0;
 }
@@ -15,19 +15,24 @@ int command(){
   printf("_$ "); //fill _ with something
   char s[1024];
   fgets(s,sizeof(s),stdin);
-  int n = 0;
+  int n = 2;
   char *p = s;
   while (*p){
     if (*p == ' '){
       n++;
     }
     p++;
-  }//remove newline
-  char **params = (char *)malloc(64 * n);
-  n = 0;
-  while (s){
-    params[n] = strsep(s,' ');
   }
-  
+  char **params = malloc(sizeof(char *) * n);
+  p = s;
+  *strchr(p,'\n') = '\0';
+  n = 0;
+  char *k;
+  while (k = strsep(&p," ")){
+	params[n] = k;
+	n++;
+  }
+  params[n] = NULL;
+  execvp(params[0],params);
   return 0;
 }
