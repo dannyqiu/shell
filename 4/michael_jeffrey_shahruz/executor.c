e5b37e93df786ca7a425ac664f28233ebdb7adc7
Jeffrey He <jh1676@gmail.com>
2014-11-21 10:54:17 -0500

    chagnes

---
+++ 4/michael_jeffrey_shahruz/executor.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+// Execs a function, parsing the input and running execvp
+int exec_line(char *input);
+
+
+int main() {
+  int status;
+  char s[1024];
+  char *s2;
+  char *s1;
+  char *first_arg;
+  char *scpy;
+  int i;
+  char *commands[1024];
+  
+  while(1) {
+    printf("^_^: ");
+    fgets(s,sizeof(s),stdin);
+    int f = fork();
+
+    scpy = malloc(1024);
+    strcpy(scpy,s);
+
+    // this part parses the input by ";" and puts each command in commands
+    s1 = s;
+    for(i=0;s1;i++){
+      s2 = strsep(&s1,";");
+      commands[i]=s2;
+    }
+    //
+
+    
+    first_arg = strsep(&scpy," ");
+    printf("scpy: %s\n",scpy);
+    
+    if(strcmp("exit",first_arg) == 0) {
+      printf("exiting\n");
+      exit(0);
+    } else if (strcmp("cd",first_arg) == 0) {
+      printf("cding\n");
+    } else {
+   if(f == 0) {
+      exec_line(s);
+    }
+    else {
+      wait(&status);
+    }
+    
+  }
+}
+}
+
+int exec_line(char *s) {
+ 
+  
+  char* s2;
+  char *array[256];
+  
+  int i=0;
+  for(;s[i]!='\n';i++){
+  }
+  s[i]=0;
+  
+  char* s1=s;
+  
+  for(i=0;s1;i++){
+    s2 = strsep(&s1," ");
+    array[i]=s2;
+  }
+  array[i]=0;
+  execvp(array[0],array);
+  return 0;
+}
