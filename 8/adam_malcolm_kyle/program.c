870d49be0e86acfe095f9b553dcc113fc8d8d5fe
Malcolm Wells <malcolmtype3@nyc.rr.com>
2014-11-21 14:06:15 -0500

    messed with redirect

--- 8/adam_malcolm_kyle/program.c
+++ 8/adam_malcolm_kyle/program.c
@@ -4,8 +4,7 @@
 void process(char * start){
   //site of user input processing
   while (start){
-    execute(strsep(&start, ";"));
-      
+    execute(strsep(&start, ";"));      
   }
 }
 
@@ -24,14 +23,15 @@ void execute(char * start){
     if(*y != 0){
       if (! strcmp(">",y))
 	redir = 1;
-    
+      if( ! strcmp("<",y))
+	redir = 2;
       args[x] = y;
       x++;
     }
     
   }
   if (redir)
-    redirect(args);
+    redirect(args, redir);
   else{
     args[x -1] = strsep(&args[x-1], "\n");
     args[x]=0;
@@ -65,8 +65,20 @@ void child_process(char * args[]){
     wait(&f);
   }
 }
-void redireCt(char * args[]){
-  printf("IMPLEMENT ME PLS");
+void redirect(char * args[], int redir){
+  int c;
+  if(redir-1){
+     c = open(args[2], O_CREAT | O_RDONLY, 0644);
+     dup2(c, STDIN_FILENO);
+     execlp(args[0], args[0], NULL);
+     close(c);
+  }else{
+    c = open(args[2], O_CREAT | O_WRONLY | O_APPEND, 0644);
+    printf("%s", args[2]);
+    dup2(c, STDOUT_FILENO);
+    execlp(args[0], args[0], NULL);
+    close(c);
+  }
 }
 
 int main(int argc, char *argv[]){
--- 8/adam_malcolm_kyle/program.h
+++ 8/adam_malcolm_kyle/program.h
@@ -2,10 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <fcntl.h>
 
 
 void process(char * start);
 void execute(char * start);
 void child_process(char * args[]);
 void normal_process(char * args[]);
+void redirect(char * args[], int redir);
