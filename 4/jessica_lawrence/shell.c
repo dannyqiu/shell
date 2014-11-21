d9a20ddabd45a7e9b4affb93d166d41fc5359edb
OokamiNya <42chrono@gmail.com>
2014-11-21 10:32:09 -0500

    splitting the atom

--- 4/jessica_lawrence/shell.c
+++ 4/jessica_lawrence/shell.c
@@ -32,16 +32,3 @@ void docmd(char* s) {
   exit(0);
 }
 
-int main () {
-  char s[256];
-  while (1) {
-    prompt(s);
-    int f = fork();
-    if(f == 0) {
-      docmd(s);
-    } else {
-      wait();
-    }
-  }
-  return 0;
-}
