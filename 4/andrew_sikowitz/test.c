c118a97497a477a9a749f372913b45b383976fa3
Andrew Sikowitz <nysiko@gmail.com>
2014-11-21 11:45:34 -0500

    Parsing strings and cleaning htem up

--- 4/andrew_sikowitz/test.c
+++ 4/andrew_sikowitz/test.c
@@ -4,10 +4,6 @@
 int main() {
   char * input = malloc(1024);
   fgets(input, 1024, stdin);
-  char * s = strsep(&input, " ");
-  char * s2 = strsep(&input, " ");
-  printf("%d\n", strcmp(s2, ""));
-  printf("You said: %s,%s,%s\n", s, s2, input);
-  free(s);
+  printf("You said: %s\n", input);
   return 0;
 }
