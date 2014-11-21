7714ca0c74d316169a75b7919f25c48b7039c553
Max Zhou <ball908765@gmail.com>
2014-11-21 13:20:48 -0500

    I hate pointers

--- 8/derek_max/shell.c
+++ 8/derek_max/shell.c
@@ -15,31 +15,33 @@ int main(){
   //fgets (s, 256, stdin);
   char* s1 = s;
   //char** commands = calloc(5, 256);
-  char* commands[] = {"ls -al", "echo hi", "exit"};
+  char* commands[] = {"ls -al", "echo hi", "exit", "\0"};
   //commands is null-terminated
   //parse here into commands
   int i = 0;
   //  commands[0] = "ls -al";
   //  commands[1] = "echo hi";
   //  commands[2] = "exit";
-  while(commands[i]){
+  while(*commands[i]){
     printf("commands[%d]:%s\n", i, commands[i]);
-    strstr("exitls -al", "exit");
+    printf("wtf booleans %d\n", *commands[i]);
     strstr("exitls -al", "exit");
     printf("%s\n", strstr("exitls -al", "exit"));
+    printf("%d\n", 9999999);
 
-    //    printf("%s", b);
-	   //    printf("%s\n", strstr(commands[i], "exit"));    
+    
+    if(*commands[i]){
+      printf("hi");
+      if(strlen(strstr(commands[i], "exit")) == strlen(commands[i])){ //only if "exit" is in the very beginning will there be no difference
+	//have to add special case for no input I think...?
 
-
-    printf("%d", 9999999);
-    if(strlen(strstr(commands[i], "exit")) == strlen(commands[i])){ //only if "exit" is in the very beginning will there be no difference
-      //have to add special case for no input I think...?
-
-      printf("Exiting.\n");
-      exit(EXIT_SUCCESS);
+	printf("Exiting.\n");
+	exit(EXIT_SUCCESS);
+      }
     }
+    
     execute(commands[i]);
+    
     i++;
   }
 }
